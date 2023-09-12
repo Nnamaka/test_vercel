3a9df8e46452e165d58a66d2f4675e561a6b1da9
Nnamaka <nnamaka7@gmail.com>
2023-09-12 15:51:53 +0100

    save

--- api/index.py
+++ api/index.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
