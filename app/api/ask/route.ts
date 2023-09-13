af9c89bd071c618d50c802f398525b0c5e6a7551
Nnamaka <nnamaka7@gmail.com>
2023-09-13 08:49:13 +0100

    save

--- app/api/ask/route.ts
+++ app/api/ask/route.ts
@@ -13,7 +13,7 @@ export async function GET(
     if (process.env.NODE_ENV === 'development'){
         domain = `${process.env.VERCEL_URL}:5328/api/python`
     } else{
-        domain = `https://${process.env.VERCEL_URL}/api/python`;
+        domain = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/python`;
     }
 
     // const response = await axios.get(`https://${domain}/api/python`)
